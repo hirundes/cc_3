dc10c1fbef31db79fbfb1f99280834bd24ce1b4d
HIRUN <hirundesilva97@gmail.com>
2025-01-29 10:49:27 -0500

    Task 5 - Inventory Management System

--- cc_3.js
+++ cc_3.js
@@ -1,15 +1,17 @@
 // Task 1 - Product Price Management Scenario
 let prices = [20, 21, 22, 23, 24];
-prices.push(25);
-prices.shift();
-console.log("Product Price Management:", prices)
+prices.push(25);    //added new price 
+prices.shift();     //removed the first price from the array
+console.log("Task 1")   //logged the prices
+console.log("   Product Price Management:", prices)
 
 //Task 2 - Modifying Customer Orders Scenario
-let orders = [1, 2, 3, 4, 5];
-orders[2] += 5;
-let totalOrders = orders.reduce((sum, quantity) => sum + quantity, 0); 
-console.log("Updated Array with Third Order Quantity Increase", orders) 
-console.log("Total Order Count", totalOrders) 
+let orders = [1, 2, 3, 4, 5]; 
+orders[2] += 5;   //increased third order quantity by 5
+let totalOrders = orders.reduce((sum, quantity) => sum + quantity, 0);    //calculated totalOrders
+console.log("Task 2") //logged orders and total orders
+console.log("   Updated Array with Third Order Quantity Increase", orders) 
+console.log("   Total Order Count", totalOrders) 
 
 //Task 3 - Employee Performance Tracking Scenario
 let employee = {
@@ -17,10 +19,11 @@ let employee = {
     role: "Leader",
     performanceScore: 25,
     isActive: true 
-}; 
-employee.performanceScore = 45;
-employee.promotionEligible = true
-console.log("Updated Employee Object", employee)
+};    //declared employee with properties 
+employee.performanceScore = 45;   //updated performanceScore
+employee.promotionEligible = true   //added new promotionEligible
+console.log("Task 3")   //logged employee
+console.log("   Updated Employee Object", employee)
 
 //Task 4 - Customer Feedback Records Scenario
 let feedback = [
@@ -40,12 +43,29 @@ let feedback = [
       feedbackText: "Service was amazing.",
       rating: 75, 
     }
-]
+]   // declared feedback array with three objects
 
 feedback.push({
   customerName: "Jorge",
   feedbackText: "The service was chill",
   rating: 100,                     
-});
-console.log("Max, Karen, Finn, and Jorge Feedback", feedback)
+});   //added new feedback object to array
+console.log("Task 4")   //logged feedback
+console.log("   Max, Karen, Finn, and Jorge Feedback", feedback)
+
+//Task 5 - Inventory Management System Scenario
+let inventory = {
+  itemName: "Sugar",
+  stockCount: 25,
+  price: 5,
+  calculateTotalValue: function() {
+    return this.stockCount * this.price;
+}
+};    //declated inventory and added method to calculateTotalValue
+
+console.log("Task 5")   //logged each part of inventory
+console.log("   Item Name", inventory.itemName)
+console.log("   Item Stock Count", inventory.stockCount)
+console.log("   Item Price", inventory.price)
+console.log("   Item Total Value", inventory.calculateTotalValue());
 
