98b5776c5327d77bb75369094f99517d9c576ad0
HIRUN <hirundesilva97@gmail.com>
2025-01-29 10:22:50 -0500

    Task 4 - Customer Feedback Records

--- cc_3.js
+++ cc_3.js
@@ -22,6 +22,30 @@ employee.performanceScore = 45;
 employee.promotionEligible = true
 console.log("Updated Employee Object", employee)
 
+//Task 4 - Customer Feedback Records Scenario
+let feedback = [
+    {
+      customerName: "Max",
+      feedbackText: "Service was decent but could use some work",
+      rating: 20,
+    },
 
+    {
+      customerName:"Karen",
+      feedbackText:  "Service was horrible.",
+      rating: 10,
+    },
+    {
+      customerName:"Finn",
+      feedbackText: "Service was amazing.",
+      rating: 75, 
+    }
+]
 
+feedback.push({
+  customerName: "Jorge",
+  feedbackText: "The service was chill",
+  rating: 100,                     
+});
+console.log("Max, Karen, Finn, and Jorge Feedback", feedback)
 
