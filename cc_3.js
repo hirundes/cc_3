b521e4e69928c62b9b9af6205590ee68a244b935
HIRUN <hirundesilva97@gmail.com>
2025-01-28 23:41:33 -0500

    Task 3 - Employee Performance Tracking

--- cc_3.js
+++ cc_3.js
@@ -11,6 +11,16 @@ let totalOrders = orders.reduce((sum, quantity) => sum + quantity, 0);
 console.log("Updated Array with Third Order Quantity Increase", orders) 
 console.log("Total Order Count", totalOrders) 
 
+//Task 3 - Employee Performance Tracking Scenario
+let employee = {
+    name: "Jackson",
+    role: "Leader",
+    performanceScore: 25,
+    isActive: true 
+}; 
+employee.performanceScore = 45;
+employee.promotionEligible = true
+console.log("Updated Employee Object", employee)
 
 
 
