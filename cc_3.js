f0deb7b33d180e3cb758881f460cdf39ff4d03d5
HIRUN <hirundesilva97@gmail.com>
2025-01-28 23:32:48 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -4,5 +4,14 @@ prices.push(25);
 prices.shift();
 console.log("Product Price Management:", prices)
 
+//Task 2 - Modifying Customer Orders Scenario
+let orders = [1, 2, 3, 4, 5];
+orders[2] += 5;
+let totalOrders = orders.reduce((sum, quantity) => sum + quantity, 0); 
+console.log("Updated Array with Third Order Quantity Increase", orders) 
+console.log("Total Order Count", totalOrders) 
+
+
+
 
 
